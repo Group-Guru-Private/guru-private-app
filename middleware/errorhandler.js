37bed6d76b4de60c60ed628bb0769c05d7b5f9a0
RizkyMaulita <rizky.maulita@gmail.com>
2021-01-22 21:58:10 +0700

    sementara tdd untuk order done kemungkinan akan ada perubahan jika kurang

--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -1,3 +1,7 @@
 module.exports = (err, req, res, next) => {
-  
+  if(err.name === 'Validation Error'){
+    const messages = err.message.map((e)=> (e.message))
+    res.status(err.status).json({messages}) 
+  } if (err.status) res.status(err.status).json({ message: err.message })
+  else res.status(500).json({ message: 'Internal Server Error' })
 }
---
+++ test/detailOrder.test.js
@@ -0,0 +1,100 @@
+const request = require('supertest')
+const app = require('../app')
+const {Student, Teacher, Order} = require('../models')
+const {generatePassword} = require('../helpers/passwordHelper')
+
+const registStudent = {
+  name: 'Student2',
+  email: 'student2@mail.com',
+  password: generatePassword('123456'),
+  role: 'student',
+  address: 'Jl. Mangga harum manis',
+  position: [-6.200000, 106.816666],
+  telpon_number: '08123456789'
+}
+
+const registTeacher =  {
+  name: 'Teacher',
+  email: 'teacher@mail.com',
+  password: generatePassword('123456'),
+  role: 'teacher',
+  address: 'Jl. Mangga harum manis',
+  position: [-6.200000, 106.816666],
+  telpon_number: '08123456789',
+  subjects: ['Mathematics', 'Chemistry'],
+  background: 'Universitas ABC, S1 Mathematics',
+  price: 100000,
+  rating: 4,
+  income: 0,
+  available_status: false,
+}
+
+let id_student = ''
+let id_teacher = ''
+let id_order = ''
+beforeAll( async (done) => {
+  try {
+    const dataStudent = await Student.create(registStudent)
+    const dataTeacher = await Teacher.create(registTeacher)
+    if (dataStudent && dataTeacher ) {
+      id_student = dataStudent.id
+      id_teacher = dataTeacher.id
+      const payload = {
+        StudentId: dataStudent.id,
+        TeacherId: dataTeacher.id,
+        subject: 'Mathematics',
+        distance: 5,
+        date: '2020-02-02',
+        total_price: (5 * 5000) + dataTeacher.price
+      }
+      const result = await Order.create(payload)
+      if (result) id_order = result.id
+      done()
+    }
+  } catch (err) {
+    done(err)
+  }
+})
+
+afterAll( async (done) => {
+  try {
+    await Student.destroy({ where: {id: id_student }})
+    await Teacher.destroy({ where: {id: id_teacher }})
+    await Order.destroy({ where: {id: id_order }})
+    done()
+  } catch (err) {
+    done(err)
+  }
+})
+
+describe('GET /orders/:id', () => {
+  describe('Success get order by id', () => {
+    test('should response with data', (done) => {
+      request(app)
+        .get('/orders/' + id_order)
+        .end((err, res) => {
+          const { body, status } = res
+          if(err) return done(err)
+          expect(status).toBe(200)
+          expect(body).toHaveProperty('id', id_order)
+          expect(body).toHaveProperty('StudentId', id_student)
+          expect(body).toHaveProperty('TeacherId', id_teacher)
+          done()
+        })    
+    })
+  })
+
+  describe('Error get order because data not found', () => {
+    test('should response with data', (done) => {
+      request(app)
+        .get('/orders/' + id_order + 5)
+        .end((err, res) => {
+          const { body, status } = res
+          if(err) return done(err)
+          expect(status).toBe(404)
+          expect(body).toHaveProperty('message', 'Data Not Found')
+          done()
+        })    
+    })
+  })
+})
